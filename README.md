237e9ad0979645c0c750f594181805e33ef04d58
Surat Mukker <surat@immersa.co>
2022-04-28 12:26:43 -0700

    Switch from python to bash

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 Creates a github app token that can be used to make Github API calls from Github Actions and other Github Apps
 
 **Acknowledgements of resources used**
-
-https://dev.to/dtinth/authenticating-as-a-github-app-in-a-github-actions-workflow-27co
-https://jacobtomlinson.dev/posts/2019/creating-github-actions-in-python/
-https://www.cloudsavvyit.com/14081/how-to-pass-environment-variables-to-docker-containers/
-
-Various https://stackoverflow.com/ discussions
+Bash script adapted from https://voximplant.com/docs/guides/managementapi/jwt
---
+++ gha_token_generator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%s"
+}
+
+payload=$( jq -n \
+    --arg iat "$(timestamp)" \
+    --arg iss "$INPUT_APPID" \
+    --arg exp "$(($(timestamp)+600))" \
+    '{
+        "iat": $iat | tonumber,
+        "iss": $iss | tonumber,
+        "exp": $exp | tonumber
+    }'
+)
+
+set -o pipefail
+
+header_template=$( jq -n \
+    --arg typ "JWT" \
+    --arg alg "RS256" \
+    '{typ: $typ, alg: $alg}'
+)
+
+b64enc() { openssl enc -base64 -A | tr '+/' '-_' | tr -d '='; }
+json() { jq -c . | LC_CTYPE=C tr -d '\n'; }
+rs_sign() { openssl dgst -binary -sha"${1}" -sign <(printf '%s\n' "$2"); }
+
+get_jwt_token() {
+  algo=RS256
+  header=$header_template || return
+  signed_content="$(json <<<"$header" | b64enc).$(json <<<"$payload" | b64enc)"
+  sig=$(printf %s "$signed_content" | rs_sign "${algo#RS}" "$INPUT_PRIVATEKEY" | b64enc)
+  printf '%s.%s\n' "${signed_content}" "${sig}"
+}
+
+fetch_gha_token() {
+  jwt_token=$(get_jwt_token)
+  response=$(curl -s -X POST \
+             -H "Authorization: Bearer $jwt_token" \
+             -H "Accept: application/vnd.github.v3+json" \
+             https://api.github.com/app/installations/"$INPUT_INSTALLID"/access_tokens)
+  token=$(echo $response | jq -r .token)
+  printf '%s\n' "${token}"
+}
+
+fetch_gha_token
